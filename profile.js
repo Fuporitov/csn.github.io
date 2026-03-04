3ece38978a0d896cf4f49d3b39026b2f6bdc7b61
Fuporitov <168258469+Fuporitov@users.noreply.github.com>
2026-03-04 22:53:21 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -22,7 +22,7 @@ setTimeout(() => {
                 currentUser.rank ? `Ранг: ${currentUser.rank}` : 'Ранг не указан';
         }
     }
-}, 5000); // 5 секунд на изучение консоли
+}, 50000); // 5 секунд на изучение консоли
 
 // ===== НАСТРОЙКИ (ССЫЛКУ МЕНЯЙ ЗДЕСЬ) =====
 const REPORT_URL = 'https://forum.blackrussia.online/threads/%D0%A4%D0%A1%D0%91-%D0%A0%D0%B0%D0%BF%D0%BE%D1%80%D1%82-%D0%BD%D0%B0-%D0%B2%D1%81%D1%82%D1%83%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-%D0%A6%D0%A1%D0%9D.14742722/page-2';   // ← СЮДА ВСТАВЛЯЕШЬ СВОЮ ССЫЛКУ
@@ -69,3 +69,4 @@ if (!currentUser) {
         currentUser.rank ? `Ранг: ${currentUser.rank}` : 'Ранг не указан';
 
 }
+
